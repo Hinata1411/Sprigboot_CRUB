649e2708640ca0534265e64f43fdd4ef39a92378
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-04-26 22:56:34 -0600

    Se agregó el archivo imports.sql

---
+++ src/main/resources/imports.sql
@@ -0,0 +1,2 @@
+INSERT INTO estudiantes (nombre, apellido, edad) VALUES ('Juan', 'Perez', 20);
+INSERT INTO estudiantes (nombre, apellido, edad) VALUES ('María', 'González', 22);
--- src/test/java/com/api/crud/CrudApplicationTests.java
+++ src/test/java/com/api/crud/CrudApplicationTests.java
@@ -1,6 +1,7 @@
 package com.api.crud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class CrudApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
